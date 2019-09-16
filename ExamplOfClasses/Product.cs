d8d7f061138ede4bfc0f35961656861d097d55bb
jpatrickkelley <jpatrickkelly205@gmail.com>
2019-09-16 14:23:35 -0500

    completed

---
+++ ExamplOfClasses/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamplOfClasses
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+
+
+    }
+}
---
+++ ExamplOfClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamplOfClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
